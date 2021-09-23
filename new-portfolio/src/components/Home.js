e173ad15b5dda1fe54311c74d76a42a2bfa915e7
Buzzaw91 <aleksi.ylonen2@gmail.com>
2021-09-23 17:55:09 +0300

    feat: home component

--- new-portfolio/src/components/Home.js
+++ new-portfolio/src/components/Home.js
@@ -1,8 +1,17 @@
 import React from 'react';
-
+import image from '../images/awar-meman-l-Qc9gcTTUI-unsplash.jpg'
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <main>
+            <img
+                src={image}
+                alt=''
+                className='absolute object-cover w-full h-full'
+                />
+            <section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
+                <h1 className='text 6xl text-indigo-100 font-bold cursive leading-none lg:leading-snug home-name'>Hi! I'm Aleksi</h1>
+            </section>
+        </main>
      );
 }
 
